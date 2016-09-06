a4ded8c5cb6f8fab313214632a293ebb5cdb5eb3
Alex <fastex93@gmail.com>
2016-09-06 15:02:21 +0500

    presentation commit

--- src/blocks/gallery/gallery.js
+++
@@ -1,6 +0,0 @@
-$(() => {
-	$('.gallery__likes').on('click', function() {
-		const likesElm = $(this).find('.gallery__value');
-		likesElm.text(parseInt(likesElm.text()) + 1);
-	});
-});
--- src/blocks/modal/modal.js
+++
@@ -1,7 +0,0 @@
-$(() => {
-	$('.modal__ok').on('click', e => {
-		e.preventDefault();
-		$('.modal__overlay').hide();
-		$('.modal__box').hide();
-	});
-});
